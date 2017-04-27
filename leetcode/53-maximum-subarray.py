b856203b3b9b33725e8baec0e6122348c12d5455
Chris McLennon <mclennon@indeed.com>
2017-04-27 14:50:32 -0700

    leetcode

---
+++ leetcode/53-maximum-subarray.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        # Using Kadane's Algorithm
+        max_ending_here = max_so_far = nums[0]
+        for x in nums[1:]:
+            max_ending_here = max(x, max_ending_here + x)
+            max_so_far = max(max_so_far, max_ending_here)
+        return max_so_far
+
+print Solution.maxSubArray(Solution(), [-2,1,-3,4,-1,2,1,-5,4])
