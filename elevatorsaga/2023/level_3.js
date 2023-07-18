0f18107629146a76651450a3320087bf5c125b7e
Chris McLennon <chris.mclennon>
2023-07-18 16:43:29 -0400

    elevatorsaga/2023/3

---
+++ elevatorsaga/2023/level_3.js
@@ -0,0 +1,17 @@
+{
+    init: function(elevators, floors) {
+        var elevator = elevators[0];
+
+	// I was surprised this also worked on level 3
+        elevator.on("idle", function() {
+            elevator.goToFloor(0);
+            elevator.goToFloor(1);
+            elevator.goToFloor(2);
+            elevator.goToFloor(3);
+            elevator.goToFloor(4);
+        });
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
+}
